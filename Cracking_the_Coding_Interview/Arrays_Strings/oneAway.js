790f79a173880568ff21074714ff54d5155beee1
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-08-12 15:42:29 -0700

    string comparison

--- Cracking_the_Coding_Interview/Arrays_Strings/oneAway.js
+++ Cracking_the_Coding_Interview/Arrays_Strings/oneAway.js
@@ -1,35 +1,45 @@
-//
-function oneAway(str1, str2){
-    if(str1 === str2) return true; // zero edits.
-
-    const lenDif = str1.length - str2.length;
-    if(Math.abs(lenDif) > 1) return false; // check only if the lengths are equal or one apart.
-
-    //find longer str.
-    let longStr = str1;
-    let shortStr = str2;
-    if(lenDif < 1) {
-        longStr = str2;
-        shortStr = str1;
+// O(N) time | O(1) space
+// The strings length defines what actions to check.
+function oneAway(first, second){
+    if(first.length === second.length){
+        return checkEditReplace(first, second);
+    } else if(first.length - 1 === second.length){
+        return checkEditInsert(second, first);
+    } else if(first.length + 1 === second.length){
+        return checkEditInsert(first, second);
     }
-
-    let edits = 0;
-    if(Math.abs(lenDif) === 1){
-
-        for(let i = 0; i < longStr.length; i++){
-            if(longStr.splice(i) === shortStr) return true;
-        }
-
-    } else {
-
-        for(let i=0; i < str1.length; i++){
-            if(str1[i] !== str2[i]) edits++;
-            if(edits > 1) return false;
-        }
-
-        return true;
-
-    }
-
     return false;
 }
+
+// check if inserting one letter in first can give us second. O(n) time | O(1) space
+function checkEditInsert(first, second){
+    let index1 = 0;
+    let index2 = 0;
+    while(index1 < first.length && index2 < second.length){
+        if(first[index1] !== second[index2]){
+            if(index1 != index2){ //catches replacement if any.
+                return false;
+            }
+            index2++;
+        } else {
+            index1++;
+            index2++;
+        }
+    }
+    return true;
+}
+// first and second lengths are equal. O(n) time | O(1) space
+function checkEditReplace(first, second){
+    let edits = 0;
+    for(let i = 0; i < first.length; i++){
+        if(first[i] !== second[i]) edits++;
+        if(edits > 1) return false;
+    }
+    return true;
+}
+
+// Tests:
+console.log(oneAway('pale','ple'), true);
+console.log(oneAway('pale','bale'), true);
+console.log(oneAway('pale','bake'), false);
+console.log(oneAway('msk','matk'), false);
