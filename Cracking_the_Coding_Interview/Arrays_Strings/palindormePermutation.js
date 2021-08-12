65fd06e0a2739510a9d42e434ac2dc0b79977b6a
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-08-12 12:54:05 -0700

    palindrome and permutation alternate

--- Cracking_the_Coding_Interview/Arrays_Strings/palindormePermutation.js
+++ Cracking_the_Coding_Interview/Arrays_Strings/palindormePermutation.js
@@ -9,11 +9,33 @@ function palindromePermutation(str){
     }
     let counter = 0;
     for(let key in letters){ //O(1) < 128 ASCII chars, if case insensitive.
-        if(letters[key] % 2 !== 0) counter++;
+        if(letters[key] % 2 === 1) counter++;
         if(counter > 1) return false;
     }
     return true;
 }
+
+// O(N) time | O(1) space, decreased to one for loop
+function palindromePermutation(str){
+    const letters = {};
+    let counter = 0;
+    for(let char of str){ 
+        if(char === ' ') continue;
+        if(char !== char.toLowerCase()) char = char.toLowerCase();
+        if(letters[char]){
+            letters[char] += 1;
+        } else {
+            letters[char] = 1;
+        }
+        if(letters[char] % 2 === 1){
+            counter++; 
+        } else {
+            counter--;
+        }
+    }
+    return counter <= 1;
+}
+
 
 // Tests:
 console.log(palindromePermutation('Tact Coa'), true);
