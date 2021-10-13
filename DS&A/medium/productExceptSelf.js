30aa84f188fb46109b944c02bbc8de1a636de839
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-10-13 14:19:08 -0700

    array product except self

---
+++ DS&A/medium/productExceptSelf.js
@@ -0,0 +1,19 @@
+// O(n2) time | O(1) space
+// var productExceptSelf = function(nums) {
+//     const answer = [];
+//     let product;
+//     for(let i = 0 ; i < nums.length; i++){
+//         product = 1;
+//         for(let j = 0; j < nums.length; j++){
+//             if(j !== i) product *= nums[j];
+//         }
+//         answer[i] = product;
+//     }
+//     return answer;
+// };
+
+//O(n) time | O() space
+var productExceptSelf = function(nums){
+    
+    
+}
