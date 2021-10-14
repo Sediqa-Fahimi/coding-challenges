d1bafe0c905ddfa3ee6796ed1e6203c29fe40e7d
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-10-14 16:46:04 -0700

    productExceptSelf

--- DS&A/medium/productExceptSelf.js
+++ DS&A/medium/productExceptSelf.js
@@ -12,8 +12,42 @@
 //     return answer;
 // };
 
-//O(n) time | O() space
+//O(n) time | O(n) space
+// var productExceptSelf = function(nums){
+//     const answer = [];
+//     const left = [];
+//     const right = [];
+    
+//     left[0] = 1;
+//     for(let i = 1; i < nums.length; i++){
+//         left[i] = left[i-1] * nums[i-1];
+//     }
+    
+//     right[nums.length - 1] = 1;
+//     for(let i = nums.length - 2 ; i >= 0 ; i--){
+//         right[i] = right[i+1] * nums[i+1];
+//     }
+    
+//     for(let i = 0; i < nums.length; i++){
+//         answer[i] = left[i] * right[i];
+//     }
+    
+//     return answer;
+// }
+
+// O(n) time | O(1) space
 var productExceptSelf = function(nums){
+    const answer = [];
     
+    answer[0] = 1;
+    for(let i = 1; i < nums.length; i++){
+        answer[i] = answer[i-1] * nums[i-1];
+    }
     
+    let right = 1;
+    for(let i = nums.length-1; i >= 0; i--){
+        answer[i] = answer[i] * right;
+        right = right * nums[i];
+    }
+    return answer;
 }
