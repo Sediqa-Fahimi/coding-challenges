0c6eea4be003b2c552f75d98a6a202a8d1ebae1a
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-10-19 00:13:13 -0700

    maxsubarray product optimal solution

--- DS&A/medium/maxSubArrayProduct.js
+++ DS&A/medium/maxSubArrayProduct.js
@@ -10,3 +10,23 @@ var maxProduct = function(nums) {
     }
     return maxPro;
 };
+
+// O(n) time | O(1) space
+var maxProduct = function(nums){
+    if(nums.length === 0) return 0;
+    
+    let maxSoFar = nums[0];
+    let minSoFar = nums[0];
+    let result = maxSoFar;
+    
+    for(let i = 1; i < nums.length; i++){
+        let curr = nums[i];
+        let tempMax = Math.max(curr, maxSoFar * curr, minSoFar * curr);
+        minSoFar = Math.min(curr, maxSoFar * curr, minSoFar * curr);
+        
+        maxSoFar = tempMax;
+        
+        result = Math.max(maxSoFar, result);
+    }
+    return result;
+}
