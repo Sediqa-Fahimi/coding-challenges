1dd535ca980d9db234a4f3973603ae7b1e597a3f
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-10-22 15:20:32 -0700

    binary search rotated sorted array

--- DS&A/medium/searchRotatedSortedArray.js
+++ DS&A/medium/searchRotatedSortedArray.js
@@ -1,3 +1,4 @@
+//O(logn) time | O(1) space
 function MinItemIndex(nums){
     let left = 0;
     let right = nums.length - 1;
@@ -23,4 +24,51 @@ function MinItemIndex(nums){
     return rotationIndex;
 }
 
-console.log(MinItemIndex([3,4,5,6,7,8,9,0,1,2]));
+// console.log(MinItemIndex([1,2,3,0]));
+
+//O(log n) time | O(1) space
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    let rotationIndex = 0;
+    
+    if(nums.length > 1){
+        
+        while(left <= right){
+            let mid = Math.floor((left + right) / 2);
+            
+            if(nums[mid] > nums[mid+1]){
+                rotationIndex = mid+1;
+                break;
+            }
+            if(nums[mid] < nums[mid-1]){
+                rotationIndex = mid;
+                break;
+            }
+            if(nums[mid] > nums[left]){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+    }
+    left = 0;
+    right = nums.length - 1;
+
+    if(rotationIndex !== 0){
+        if(target > nums[0]) right = rotationIndex - 1;
+        if(target < nums[0]) left = rotationIndex;
+    }
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        
+        if(nums[mid] === target) return mid;
+        
+        if(target > nums[mid]) left = mid + 1;
+        if(target < nums[mid]) right = mid - 1;
+    }
+    return -1;
+};
+
+console.log(search([2,3,4,5,0,1],9));
