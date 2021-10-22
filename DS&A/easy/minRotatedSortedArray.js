ae9cd2361b244b7bb10abf7a9915f90232bfcf2f
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-10-21 23:01:54 -0700

    min in rotated sorted array optimized

--- DS&A/easy/minRotatedSortedArray.js
+++ DS&A/easy/minRotatedSortedArray.js
@@ -14,3 +14,27 @@
 //     }
 //     return nums[0];
 // };
+
+// O(logn) time | O(1) space 
+// Binary search
+var findMin = function(nums) {
+    if(nums.length === 1) return nums[0];
+    
+    let left = 0;
+    let right = nums.length - 1;
+    
+    if(nums[right] > nums[0]) return nums[0];
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        
+        if(nums[mid] > nums[mid+1]) return nums[mid+1];
+        if(nums[mid] < nums[mid-1]) return nums[mid];
+        
+        if(nums[mid] > nums[left]){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+};
