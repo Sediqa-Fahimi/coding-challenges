52cc63e4af58eb0522f66b8eacf663cda84ce68c
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-10-21 23:34:20 -0700

    Optimized binary search

--- DS&A/easy/binarySearch.js
+++ DS&A/easy/binarySearch.js
@@ -10,3 +10,22 @@ function binarySearch(array, target) {
 		return midIdx;
 	}
 }
+
+// O(log n) time | O(1) space
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        
+        if(nums[mid] === target) return mid;
+        
+        if(target > nums[mid]){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
