fa9ef1a128e55e4a6f7841d54c982d0757c72d75
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-02-07 15:51:48 -0800

    used template literals

--- JS/classes/script.js
+++ JS/classes/script.js
@@ -11,8 +11,18 @@ import Desk from './Desk.js';
 
 
 const myDesk = new Desk('desk1','brown');
-console.log(myDesk.name, myDesk.color);
-
-myDesk.updateNameColor('newname','black');
 
 console.log(myDesk.name, myDesk.color);
+myDesk.updateNameColor('Work Desk','black');
+console.log(myDesk.name, myDesk.color);
+
+// Template Literal: mix of HTML and JS.
+const content = `
+    <p>Hello , You Got This!</p>
+    <ul>
+        <li>Name: ${myDesk.name}</li>
+        <li>Color: ${myDesk.color}</li>
+    </ul>
+`;
+
+document.body.innerHTML = content;
