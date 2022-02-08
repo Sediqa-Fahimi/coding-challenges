93320d0836f6c781d8d9efe622a3c2436230fec5
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-02-07 18:26:25 -0800

    DOM elements attributes

---
+++ JS/DOM manipulation/script.js
@@ -0,0 +1,9 @@
+/**
+ * Note: This file is intentionally empty.
+ * You can use it to test your skills at adding, removing, or modifying
+ * attributes, classes, and inline styles on elements.
+ */
+document.querySelector('header').classList.add('class-1','class-2');
+
+document.querySelector('header').classList.remove('class-2');
+
