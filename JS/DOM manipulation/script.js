e69a4d91f52f6490d685f6ffa963e7a36ce84179
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-02-07 18:57:04 -0800

    Add new element to DOM

--- JS/DOM manipulation/script.js
+++ JS/DOM manipulation/script.js
@@ -7,3 +7,21 @@ document.querySelector('header').classList.add('class-1','class-2');
 
 document.querySelector('header').classList.remove('class-2');
 
+const content = `
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+`;
+
+const nav = document.createElement('nav');
+nav.classList.add('navbar');
+nav.innerHTML = content;
+const header = document.querySelector('header');
+header.insertAdjacentElement('afterend', nav);
+
+
+
