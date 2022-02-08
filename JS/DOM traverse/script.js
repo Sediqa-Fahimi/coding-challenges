4d78b9dc917805bbbadd18237fdd0041c84c0832
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-02-07 17:02:34 -0800

    Accessing DOM elements

--- JS/DOM traverse/script.js
+++ JS/DOM traverse/script.js
@@ -2,3 +2,11 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+
+
+const header = document.querySelector('header');
+header.style.backgroundColor = 'blue';
+
+const lis = document.querySelectorAll('li');
+lis.forEach(item => item.style.listStyleType = 'none');
