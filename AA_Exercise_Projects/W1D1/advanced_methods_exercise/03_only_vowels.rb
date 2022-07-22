7b7c3ce9310880d496f4dbe3a8098740a3beb5eb
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-07-21 18:17:12 -0700

    using array.all?

--- AA_Exercise_Projects/W1D1/advanced_methods_exercise/03_only_vowels.rb
+++ AA_Exercise_Projects/W1D1/advanced_methods_exercise/03_only_vowels.rb
@@ -2,6 +2,18 @@
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
 
+# def only_vowels?(str)
+#     vowels = 'aeiou'
+#     return str.split("").all? {|char| vowels.include?(char) }
+# end
+
+def only_vowels?(str) # O(n)time | O(1) space
+    vowels = 'aeiou'
+    str.each_char do |char|
+        return false if !vowels.include?(char)
+    end
+    return true
+end
 
 p only_vowels?("aaoeee")  # => true
 p only_vowels?("iou")     # => true
