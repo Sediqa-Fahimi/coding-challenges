d6fa16309497cb7d60dd81f6701303162f12c770
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-07-21 19:02:04 -0700

    advanced methods

--- AA_Exercise_Projects/W1D1/advanced_methods_exercise/06_union.rb
+++ AA_Exercise_Projects/W1D1/advanced_methods_exercise/06_union.rb
@@ -1,8 +1,12 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
 
-def union(*arr)
-    return arr.flatten
+# def union(*arr)
+#     return arr.flatten
+# end
+
+def union(*arrays)
+    arrays.inject {|all_elements,arr| all_elements + arr }
 end
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
