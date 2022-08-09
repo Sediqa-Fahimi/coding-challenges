31b66b2b06926fb8bac4aec0215052512952ef67
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-08-09 15:42:35 -0700

    perilous procs phase 3

--- AA_Exercise_Projects/W1D4/prelious_procs/phase1.rb
+++ AA_Exercise_Projects/W1D4/prelious_procs/phase1.rb
@@ -264,3 +264,35 @@ end
 #     three_letters => 'o'
 # )   # "fooding gladingly rantingly dogo catlyo"
 
+def proctition_platinum(arr, *args)
+    hash = {}
+    (1..args.length).each {|n| hash[n] = []}
+
+    arr.each do |ele|
+       args.each_with_index do |proc, i|
+            if proc.call(ele)
+                hash[i+1] << ele  
+                break
+            end
+       end
+    end
+    hash
+end
+
+# is_yelled = Proc.new { |s| s[-1] == '!' }
+# is_upcase = Proc.new { |s| s.upcase == s }
+# contains_a = Proc.new { |s| s.downcase.include?('a') }
+# begins_w = Proc.new { |s| s.downcase[0] == 'w' }
+
+# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, contains_a)
+# # {1=>["when!", "WHERE!"], 2=>["what"]}
+
+# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, is_upcase, contains_a)
+# # {1=>["when!", "WHERE!"], 2=>["WHO", "WHY"], 3=>["what"]}
+
+# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcase, is_yelled, contains_a)
+# # {1=>["WHO", "WHERE!", "WHY"], 2=>["when!"], 3=>["what"]}
+
+# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
+# # {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
+
