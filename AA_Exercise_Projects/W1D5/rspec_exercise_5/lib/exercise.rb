74eb62bec01841eac22eca1f732c2247f1211fd1
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-08-10 13:28:40 -0700

    rspec exercise 5 half done

--- AA_Exercise_Projects/W1D5/rspec_exercise_5/lib/exercise.rb
+++ AA_Exercise_Projects/W1D5/rspec_exercise_5/lib/exercise.rb
@@ -35,3 +35,51 @@ def zany_zip(*args)
     end
     zip
 end
+
+def maximum(arr, &prc)
+    return nil if arr.empty?
+    final_ele = 0
+    arr.inject(0) do |accu, ele|
+        result = prc.call(ele)
+        if accu <= result
+            final_ele = ele
+            result 
+        else
+            accu
+        end
+    end
+    final_ele
+end
+
+def my_group_by(arr, &prc)
+    groups = {}
+    arr.each do |ele|
+        res = prc.call(ele)
+        groups[res] ? groups[res] << ele : groups[res] = [ele]
+    end 
+    groups
+end
+def tie_breaker(ele1, ele2, proc)
+    res1 = proc.call(ele1)
+    res2 = proc.call(ele2)
+    res1 > res2 ? [res1,ele1] : [res2,ele2]
+end
+
+def max_tie_breaker(arr, proc, &prc)
+    return nil if arr.empty?
+    final_ele = ''
+    arr.inject(0) do |accu, ele|
+        result = prc.call(ele)
+        if accu < result
+            final_ele = ele
+            result 
+        elsif accu == result
+            proc_ele = tie_breaker(ele,final_ele,proc)
+            final_ele = proc_ele[1]
+            result
+        else
+            accu
+        end
+    end
+    final_ele
+end
