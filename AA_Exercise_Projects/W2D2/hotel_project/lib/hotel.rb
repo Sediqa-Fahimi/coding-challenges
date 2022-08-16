72d86a29d9bafcb2ea56f52349a7dc32d6822bd5
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-08-15 22:02:37 -0700

    hotel project completed

--- AA_Exercise_Projects/W2D2/hotel_project/lib/hotel.rb
+++ AA_Exercise_Projects/W2D2/hotel_project/lib/hotel.rb
@@ -1,5 +1,38 @@
 require_relative "room"
 
 class Hotel
-  
+    attr_reader :rooms
+    def initialize(name, rooms)
+        @name = name
+        @rooms = {}
+        rooms.each do |k,v|
+            @rooms[k] = Room.new(v)
+        end
+    end
+    
+    def name
+        @name.split(" ").map {|word| word.capitalize}.join(" ")
+    end
+    def room_exists?(name)
+        @rooms.has_key?(name) ? true : false
+    end
+    def check_in(person, room)
+        if room_exists?(room)
+            if @rooms[room].add_occupant(person)
+                puts 'check in successful'
+            else
+                puts 'sorry, room is full'
+            end
+        else
+            puts 'sorry, room does not exist'
+        end
+    end
+    def has_vacancy?
+        @rooms.any? {|k, room| !room.full? }
+    end
+    def list_rooms
+        @rooms.each do |room_name, room|
+            puts room_name + ' ' + room.available_space.to_s
+        end
+    end
 end
--- AA_Exercise_Projects/W2D2/hotel_project/lib/room.rb
+++ AA_Exercise_Projects/W2D2/hotel_project/lib/room.rb
@@ -1,3 +1,27 @@
 class Room
-  
+    attr_reader :capacity, :occupants
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
+
+    def full?
+        if @occupants.length < @capacity
+            false
+        elsif @occupants.length == @capacity
+            true
+        end
+    end
+    def available_space
+        @capacity - @occupants.length
+    end
+
+    def add_occupant(name)
+        if full?
+            false
+        else
+            @occupants.push(name)
+            true
+        end
+    end
 end
