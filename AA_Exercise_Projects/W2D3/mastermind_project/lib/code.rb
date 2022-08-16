fce85bff5f2e7a3857789a8fc929f3f8b31a32eb
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-08-16 14:37:07 -0700

    mastermind project completed

--- AA_Exercise_Projects/W2D3/mastermind_project/lib/code.rb
+++ AA_Exercise_Projects/W2D3/mastermind_project/lib/code.rb
@@ -37,16 +37,27 @@ class Code
     end
     count
   end
-  # def num_near_matches(code)
-  #   return 0 if num_exact_matches(code) == @pegs.length
-  #   code.pegs.each_with_index do |peg, i|
-  #     if peg == @pegs[i]
+  def num_near_matches(code)
+    self_copy = self.pegs.dup
+    code_copy = code.pegs.dup
 
-  #     else
-
-  #     end
-  #   end
-  # end
+    code_copy.each_with_index do |peg,i| 
+      if peg == @pegs[i]
+        self_copy[i] = nil
+        code_copy[i] = nil
+      end
+    end
+    self_copy.delete(nil)
+    code_copy.delete(nil)
+    count = 0
+    code_copy.each_with_index do |peg,i|
+      if self_copy.include?(peg)
+        count += 1
+        self_copy.delete_at(self_copy.index(peg))
+      end
+    end
+    count
+  end
   def ==(code)
     code.pegs == self.pegs
   end
--- AA_Exercise_Projects/W2D3/mastermind_project/lib/mastermind.rb
+++ AA_Exercise_Projects/W2D3/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
-
+    def initialize(len)
+        @secret_code = Code.random(len)
+    end
+    def print_matches(code)
+        exact_matches = @secret_code.num_exact_matches(code)
+        puts 'number of exact matches:' + exact_matches.to_s
+        near_matches = @secret_code.num_near_matches(code)
+        puts 'number of near matches:' + near_matches.to_s
+    end
+    def ask_user_for_guess
+        puts 'Enter a code'
+        str = gets.chomp
+        code = Code.from_string(str)
+        print_matches(code)
+        @secret_code == code
+    end
 end
