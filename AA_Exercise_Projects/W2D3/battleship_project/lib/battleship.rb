643328b69bbe26fa9f26207a09fab2f812df2c31
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-08-18 22:12:13 -0700

    battleship project completed

--- AA_Exercise_Projects/W2D3/battleship_project/lib/battleship.rb
+++ AA_Exercise_Projects/W2D3/battleship_project/lib/battleship.rb
@@ -2,5 +2,42 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-
+    attr_reader :board, :player
+    def initialize(n)
+        @player = Player.new
+        @board = Board.new(n)
+        @remaining_misses = @board.size / 2
+    end
+    def start_game
+        @board.place_random_ships
+        puts 'There are ' + @board.num_ships.to_s + ' ships on the board.'
+        @board.print
+    end
+    def lose?
+        if @remaining_misses <= 0
+            puts 'you lose'
+            true
+        else
+            false
+        end
+    end
+    def win?
+        if @board.num_ships == 0
+            puts 'you win'
+            true
+        else
+            false
+        end
+    end
+    def game_over?
+        self.lose? || self.win?
+    end
+    def turn
+        move = @player.get_move
+        if !@board.attack(move)
+            @remaining_misses -= 1
+        end
+        @board.print
+        puts 'remaining misses are:' + @remaining_misses.to_s
+    end
 end
--- AA_Exercise_Projects/W2D3/battleship_project/lib/player.rb
+++ AA_Exercise_Projects/W2D3/battleship_project/lib/player.rb
@@ -1,3 +1,8 @@
 class Player
-
+    def get_move
+        puts 'enter a position with coordinates separated with a space like `4 7`'
+        position = gets.chomp.split(" ") #position = ['4','7']
+        position.each_with_index {|ele,i| position[i] = ele.to_i}
+        position
+    end
 end
