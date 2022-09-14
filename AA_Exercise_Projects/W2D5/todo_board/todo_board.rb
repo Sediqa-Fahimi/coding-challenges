0d963b47e5ed793902de975006fc51f98c12d209
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-09-13 21:59:52 -0700

    todo board completed

--- AA_Exercise_Projects/W2D5/todo_board/todo_board.rb
+++ AA_Exercise_Projects/W2D5/todo_board/todo_board.rb
@@ -1,46 +1,60 @@
 require_relative 'list'
 
 class TodoBoard
-    def initialize(label)
-        @list = List.new(label)
+    def initialize
+        @lists = {}
     end
     def get_command
         print "\nEnter a command:"
         cmd, *args = gets.chomp.split(" ")
-        
+        list_label = args.shift
         case cmd
+        when "mklist"
+            @lists[list_label] = List.new(list_label)
+        when "ls"
+            puts "Lists Labels"
+            puts "-------"
+            @lists.each do |k,v|
+                puts k
+            end
+        when "showall"
+            puts "All Lists"
+            puts "**********"
+            @lists.each do |k,v|
+                v.print
+            end
         when "mktodo"
-            @list.add_item(*args)
+            @lists[list_label].add_item(*args)
         when "up"
             num_args = args.map {|arg| arg.to_i}
-            @list.up(*num_args)
+            @lists[list_label].up(*num_args)
         when "down"
             num_args = args.map {|arg| arg.to_i}
-            @list.down(*num_args)
+            @lists[list_label].down(*num_args)
         when "swap"
             num_args = args.map {|arg| arg.to_i}
-            @list.swap(*num_args)
+            @lists[list_label].swap(*num_args)
         when "sort"
-            @list.sort_by_date!
+            @lists[list_label].sort_by_date!
         when "priority"
-            @list.print_priority
+            @lists[list_label].print_priority
         when "print"
             if args.empty?
-                @list.print
+                @lists[list_label].print
             else
                 num_args = args.map {|arg| arg.to_i}
-                @list.print_full_item(*num_args)
+                @lists[list_label].print_full_item(*num_args)
             end
         when "quit"
             return false
         when "toggle"
             num_args = args.map {|arg| arg.to_i}
-            @list.toggle_item(*num_args)
+            @lists[list_label].toggle_item(*num_args)
         when "rm"
             num_args = args.map {|arg| arg.to_i}
-            @list.remove_item(*num_args)
+            @lists[list_label].remove_item(*num_args)
         when "purge"
-            @list.purge
+            @lists[list_label].purge
         else
             print "Sorry, that command is not recognized."
         end
@@ -53,3 +67,7 @@ class TodoBoard
         end
     end
 end
+
+my_board = TodoBoard.new
+
+my_board.run
