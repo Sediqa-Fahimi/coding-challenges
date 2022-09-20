362bd176fadbaf8f14b034c432ac8369ca31162e
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-09-19 23:36:57 -0700

    memory puzzle render method incomplete

--- AA_Exercise_Projects/W3D2/memory_puzzle/board.rb
+++ AA_Exercise_Projects/W3D2/memory_puzzle/board.rb
@@ -1,5 +1,39 @@
 require_relative "card"
 
 class Board
+    attr_accessor :grid
+    def initialize(*cards)
+        @grid = Array.new(4) {Array.new(4)}
+        populate(cards)
+    end
+    def empty(pos)
+        row = pos[0]
+        col = pos[1]
+        @grid[row][col] == nil
+    end
+    def populate(cards)
+        cards.each do |card|
+            2.times do 
+                row = (0..3).to_a.sample
+                col = (0..3).to_a.sample
+                while !empty([row,col])
+                    row = (0..3).to_a.sample
+                    col = (0..3).to_a.sample
+                end
+                @grid[row][col] = card
+            end
+        end
+    end
+    def render
+        p "  0  1  2  3"
+        @grid.map.with_index do |row,i|
+            p "#{i}  #{row.map {|col| col.display_card}.join(" | ")}"
+        end
+    end
+    def won?
 
+    end
+    def reveal
+
+    end
 end
--- AA_Exercise_Projects/W3D2/memory_puzzle/card.rb
+++ AA_Exercise_Projects/W3D2/memory_puzzle/card.rb
@@ -1,3 +1,26 @@
 class Card
-
+    attr_reader :value
+    def initialize(value)
+        @value = value
+        @face_up = false
+    end
+    def display_card
+        if @face_up
+            @value
+        else
+            " "
+        end
+    end
+    def hide
+        @face_up = false
+    end
+    def reveal
+        @face_up = true
+    end
+    def to_s
+        @value
+    end
+    def ==(other_value)
+        @value == other_value
+    end
 end
