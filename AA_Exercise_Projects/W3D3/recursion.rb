4ed2dd6774bbfb4e61951ef72cfd221057420975
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-09-20 19:10:05 -0700

    recursion exponentiation

--- AA_Exercise_Projects/W3D3/recursion.rb
+++ AA_Exercise_Projects/W3D3/recursion.rb
@@ -8,3 +8,21 @@ end
 # p range(2,8) #=> [2,3,4,5,6,7]
 # p range(-3,3) #=> [-3,-2,-1,0,1,2]
 
+def exp1(b,n)
+    return 1 if n == 0
+    return b if n == 1
+    b * exp1(b, n-1)
+end
+
+def exp2(b,n)
+    return 1 if n == 0
+    return b if n == 1
+    if n % 2 == 0
+        exp2(b, n / 2) ** 2
+    else
+        b * (exp2(b, (n - 1) / 2) ** 2)
+    end
+end
+p exp2(3,0) #=> 1
+p exp2(4,1) #=> 4
+p exp2(2,3) #=> 8
