81dc8ab3d48c6b785208074898069afc20fd697a
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-10-07 15:01:36 -0700

    Uncomment the test cases
    Practicing working with text editors for commit messages.

--- AA_Exercise_Projects/W3D3/recursion.rb
+++ AA_Exercise_Projects/W3D3/recursion.rb
@@ -6,9 +6,9 @@ def range(start, ending)
     [start] + range(start + 1, ending)
 end 
 
-# p range(3,1) #=> []
-# p range(2,8) #=> [2,3,4,5,6,7]
-# p range(-3,3) #=> [-3,-2,-1,0,1,2]
+p range(3,1) #=> []
+p range(2,8) #=> [2,3,4,5,6,7]
+p range(-3,3) #=> [-3,-2,-1,0,1,2]
 
 def exp1(b,n)
     return 1 if n == 0
