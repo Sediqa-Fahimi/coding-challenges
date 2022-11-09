29f73905db273f05ff1b61f0901658a6efb2fade
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-11-08 18:41:06 -0800

    search a str in another longer str correct version

--- Udemy_DS_learning/stringSearch.js
+++ Udemy_DS_learning/stringSearch.js
@@ -1,23 +1,19 @@
-//O(N) time | O(1) space , N is the length of the longer str.
+//O(N*M) time | O(1) space , N is the length of the longer str, M is the length of the shorter str.
 function stringSearch(str1, str2){
-    let matches = 0;
-    let idx1 = 0;
-    let idx2 = 0;
-    while(idx1 <= str1.length){
-        if(idx2 === str2.length){
-            idx2 = 0;
-            matches += 1;
+    // assume str1 is longer than str2.
+    let count = 0;
+    for(let i = 0; i < str1.length;i++){
+        for(let j = 0; j < str2.length; j++){
+            if(str1[i+j] !== str2[j]) break;
+            if(j === str2.length - 1) count++;
         }
-        if(idx1 === str1.length) break;
-        if(str1[idx1] === str2[idx2]) idx2 += 1;
-        else idx2 = 0;
-    
-        idx1 += 1;
     }
-    return matches;
+    return count;
 }
 //return the count of str2 appearance in str1.
 
 console.log(stringSearch('hahaomgwowomg', 'omg')); // 2
+console.log(stringSearch('hahaomgwoomg', 'omg')); // 2
 console.log(stringSearch('hah', 'wow'));           // 0
 console.log(stringSearch('justastring', 'ring'));  // 1
+console.log(stringSearch('justastring notaring', 'ring'));  // 2
