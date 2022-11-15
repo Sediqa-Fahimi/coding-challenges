003aa855da60b5070aea863ec7dfd415de484c0c
SediqaFahimi <sediqa.fahimi@gmail.com>
2022-11-15 14:54:02 -0800

    implement singly linked list push

---
+++ Udemy_DS_learning/DataStructures/SinglyLinkedList/push.js
@@ -0,0 +1,33 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let list = new SinglyLinkedList();
+console.log(list.push("Hello"));
+console.log(list.push("there"));
+console.log(list.push("how"));
+console.log(list.push("are"));
+console.log(list.push("you"));
+
