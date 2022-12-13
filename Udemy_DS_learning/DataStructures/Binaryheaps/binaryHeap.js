6d763491094fa43617270c26c7cc98fbb964c9f5
Sediqa Fahimi <sediqa.fahimi@gmail.com>
2022-12-12 20:16:38 -0800

    Implement Binay Heap Extract  Max

--- Udemy_DS_learning/DataStructures/Binaryheaps/binaryHeap.js
+++ Udemy_DS_learning/DataStructures/Binaryheaps/binaryHeap.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap{
     constructor(){
-        const values = [];
+        this.values = [];
     }
     insert(value){
         this.values.push(value);
@@ -20,4 +20,47 @@ class MaxBinaryHeap{
             idx = parentIdx;
         }
     }
+    extractMax(){
+        let max = this.values[0];
+        let end = this.values.pop();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+    sinkDown(){
+        let parentIdx = 0;
+        const length = this.values.length; 
+        const element = this.values[0];
+        while(true){
+            let leftChildIdx = 2 * parentIdx + 1;
+            let rightChildIdx = 2 * parentIdx + 2;
+            let leftChild, rightChild;
+            let swapIdx = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild > element){
+                    swapIdx = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(
+                    (swapIdx === null && rightChild > element) ||
+                    (swapIdx !== null && rightChild > leftChild)
+                ){
+                    swapIdx = rightChildIdx;
+                }
+            }
+            if(swapIdx === null) break;
+            this.values[parentIdx] = this.values[swapIdx];
+            this.values[swapIdx] = element;
+            parentIdx = swapIdx;
+        }
+    }
 }
+
+
+
