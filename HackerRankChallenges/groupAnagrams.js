7578ab2c6a623a699bff6fe4705f2465c6ae5063
Sediqa Fahimi <sediqa.fahimi@gmail.com>
2023-01-18 18:36:52 -0800

    Optimize group anagrams

--- HackerRankChallenges/groupAnagrams.js
+++ HackerRankChallenges/groupAnagrams.js
@@ -10,6 +10,33 @@
 // Input: strs = ["a"]
 // Output: [["a"]]
 
+// O(N * M) time | O(N * M) space
+var groupAnagrams = function(strs){
+    const anagrams = {};
+    const count = new Array(26);
+    
+    for(let str of strs){ 
+        count.fill(0);
+        let strArr = str.split("");
+        for(let char of strArr){ 
+            count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        }
+        
+        let keyArr = [];
+        for(i = 0; i < 26; i++){
+            keyArr.push("#");
+            keyArr.push(count[i]);
+        }
+        let keyStr = keyArr.join("");
+        
+        if(!anagrams[keyStr]) anagrams[keyStr] = [str];
+        else anagrams[keyStr].push(str);
+        
+    }
+    return Object.values(anagrams);
+}
+
+// O(N * MlogM) time | O(NM) space
 function groupAnagrams(strs){
 	const groups = {};
 	for(let str of strs){
