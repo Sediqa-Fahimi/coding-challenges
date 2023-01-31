9000bd916b456fd86ea68b2ecf83c01caae70c34
Sediqa Fahimi <sediqa.fahimi@gmail.com>
2023-01-30 22:28:38 -0800

    Find all paths in tree

--- AlgoExpert/allPaths.js
+++ AlgoExpert/allPaths.js
@@ -71,3 +71,23 @@ btree.insert("F");
 */
 
 console.log(allPaths(btree.root)); // [ [ 'A', 'B', 'D' ], [ 'A', 'B', 'E' ], [ 'A', 'C', 'F' ] ]
+
+const allTreePaths = (root) => {
+    if (root === null) return [ ];
+
+    if (root.left === null && root.right === null) return [ [root.val] ]
+
+    const paths = [];
+
+    const leftSubPaths = allTreePaths(root.left);
+    for (let subPath of leftSubPaths) {
+    paths.push([ root.val, ...subPath ]);
+    }
+
+    const rightSubPaths = allTreePaths(root.right);
+    for (let subPath of rightSubPaths) {
+    paths.push([ root.val, ...subPath ]);
+    }
+
+    return paths;
+};
