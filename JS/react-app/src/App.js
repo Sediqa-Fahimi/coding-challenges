0a7cdfa16a4b0de0c05107b0ad93de2170b7cd2e
Sediqa Fahimi <sediqa.fahimi@gmail.com>
2023-04-03 13:09:10 -0700

    create custom hook

--- JS/react-app/src/App.js
+++ JS/react-app/src/App.js
@@ -1,36 +1,38 @@
-
 import './App.css';
 import {useState} from 'react';
 
-function App() {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+function useInput(initialValue){
+  const [value, setValue] = useState(initialValue);
+  return [
+    {
+      value,
+      onChange: (e) => {setValue(e.target.value)}
+    },
+    () => {setValue(initialValue)}
+  ];
+}
 
-  const titleChange = (e) =>{
-    setTitle(e.target.value);
-    console.log(title)
-  }
-  const colorChange = (e) =>{
-    setColor(e.target.value);
-  }
+function App() {
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
+
+
   const submit = (e) => {
     e.preventDefault();
-    alert(`${title}, ${color}`);
-    setTitle("");
-    setColor("#000000");
+    alert(`${titleProps.value}, ${colorProps.value}`);
+    resetTitle();
+    resetColor();
   }
   return (
     <form onSubmit={submit}>
       <input 
-      value={title}
+      {...titleProps}
       type="text" 
       placeholder="color title"
-      onChange={titleChange}
       />
       <input
-      value={color}
+      {...colorProps}
       type="color"
-      onChange={colorChange}
       />
       <input type="submit" value="Add"/>
     </form>
