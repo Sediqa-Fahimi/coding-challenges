4d9ebc403458f7425f81570112e8ae9be88f80ea
Sediqa Fahimi <sediqa.fahimi@gmail.com>
2023-05-11 13:24:34 -0700

    Explain time complexity

--- AlgoExpert/waysToClimb.js
+++ AlgoExpert/waysToClimb.js
@@ -1,3 +1,4 @@
+// O(N) time | O(N + M) space, n is the # of steps, M is the # of recursive calls.
 function waysToClimb(n, memo = {}){
   if(n in memo) return memo[n];
   if(n === 1) return 1;
@@ -6,3 +7,5 @@ function waysToClimb(n, memo = {}){
   memo[n] = waysToClimb(n-1) + waysToClimb(n-2);
   return memo[n];
 }
+
+// without memoization : O(2^n) time.
