1a22cc69b8bdc543dfe9148b24eaa9fdcf5c475d
Sediqa Fahimi <sediqa.fahimi@gmail.com>
2023-11-06 12:57:45 -0800

    query that uses cross join

---
+++ sql/queries.sql
@@ -0,0 +1,12 @@
+-- Write a solution to find the number of times each student attended each exam.
+select s.student_id, s.student_name, sub.subject_name, coalesce(grouped.attended_exams, 0) As attended_exams
+from Students s
+cross join Subjects sub
+left join (
+    select student_id, subject_name, count(*) as attended_exams
+    from Examinations
+    group by student_id, subject_name
+) grouped
+on s.student_id = grouped.student_id AND sub.subject_name = grouped.subject_name
+order by s.student_id, sub.subject_name;
+
